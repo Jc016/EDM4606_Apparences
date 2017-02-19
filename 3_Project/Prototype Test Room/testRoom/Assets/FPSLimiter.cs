151e43c9e80899e5efd3dc68c8031f32284187c0
Jean-Christophe Leclerc-Massé <jclm1992@gmail.com>
2017-02-19 09:37:45 -0500

    added Test Room project to the repository

---
+++ 3_Project/Prototype Test Room/testRoom/Assets/FPSLimiter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour {
+
+    // Use this for initialization
+    private void Awake()
+    {
+        Application.targetFrameRate = 24;
+    }
+}
