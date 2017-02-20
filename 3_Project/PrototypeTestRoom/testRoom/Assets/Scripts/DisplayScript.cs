1457e438f2a31225358bec8e1cd889792c7ce299
Jc016 <jclm1992@gmail.com>
2017-02-20 23:27:04 +0100

    added multiscreen support and kinect

---
+++ 3_Project/PrototypeTestRoom/testRoom/Assets/Scripts/DisplayScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayScript : MonoBehaviour
+{
+    void Awake()
+    {
+        Screen.SetResolution(3840, 1080, false);
+        Display.main.SetRenderingResolution(3840, 1080);
+    }
+    // Use this for initialization
+    void Start()
+    {
+ 
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
