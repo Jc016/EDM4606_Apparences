c9e093763a4491d535fb41c3d5a2378f03a9e4b4
Jc016 <jclm1992@gmail.com>
2017-03-06 20:49:05 -0500

    work in progress scene management

---
+++ 3_Project/PrototypeTestRoom/testRoom/Assets/SceneManagement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Timers;
+
+public class SceneManagement : MonoBehaviour {
+    Scene[] Scenes;
+    public enum SceneNames {Intro,Main,Void};
+    private Timer projectTimer;
+
+
+    // Use this for initialization
+    void Start () {
+        SceneManager.LoadScene(1, LoadSceneMode.Additive);
+        Invoke("LoadRoom", 7);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+      if (Input.GetKeyDown("space"))
+      {
+            RestartProject();
+      }
+    }
+
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.UnloadSceneAsync(1);
+        SceneManager.LoadScene(2,LoadSceneMode.Additive);
+    }
+
+    private void RestartProject()
+    {   
+        SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene());
+    }
+
+
+
+    private void LoadRoom()
+    {
+        LoadScene("room");
+    }
+
+    
+
+}
